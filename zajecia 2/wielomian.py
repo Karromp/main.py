c87398f8d9abb48add0f9b7e961a6f182480286f
Karromp <karolinaromp12@gmail.com>
2023-12-01 10:00:43 +0100

    wielomian 29.11.2023

--- zajecia 2/wielomian.py
+++ zajecia 2/wielomian.py
@@ -1,2 +1,12 @@
-def wielomian():
-    for i in range
+x=3
+listarg=[3,2,1,-1,-2]
+s=4
+def wielo(x,lista):
+    listarg.reverse()
+    w=1
+    wielomian=listarg[0]
+    print(wielomian)
+    for i in range(1,s+1):
+        w*= x
+        wielomian+=w*listarg[i]
+    return wielomian
